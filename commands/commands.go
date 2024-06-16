436e729331b0ae78be7582559c872326c4d74b9a
akayashuu <sauvageleo1@gmail.com>
2024-06-16 03:16:10 +0200

    feat: :fire: removed outdated file

--- commands/commands.go
+++
@@ -1,9 +0,0 @@
-package commands
-
-type Command struct {
-	Name       string
-	Aliases    []string
-	FuncToCall func()
-}
-
-var CommandsList []Command
--- commands/go.mod
+++
@@ -1,3 +0,0 @@
-module commands
-
-go 1.22.4
